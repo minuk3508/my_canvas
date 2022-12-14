31bf6eb441c0e65827d9d7df4bc1ff0030214c44
김민욱 <minuk3508@gmail.com>
2022-12-14 16:50:48 +0900

    선 굵기 기능 추가 ^_^

--- src/Template/Template.tsx
+++ src/Template/Template.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { MouseEventHandler, useCallback, useEffect, useRef, useState } from "react";
 import { ColorResult, RGBColor, SketchPicker } from "react-color";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { BiEraser } from "react-icons/bi";
+import { MdLineWeight } from "react-icons/md";
 
 type ColorPickerProps = {
   color: string;
@@ -11,14 +12,19 @@ type ModColor = {
 };
 
 const DEFAULT_COLOR = { r: 0, g: 0, b: 0, a: 1 };
+const DEFAULT_WEIGHT = 1.5;
 
 export default function Template() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const weghtModalRef = useRef<HTMLDivElement | null>(null);
+  const weghtModaltestRef = useRef<HTMLDivElement | null>(null);
   const [getCtx, setGetCtx] = useState<CanvasRenderingContext2D>();
   const [painting, setPainting] = useState<boolean>(false);
-  const [displayColorPicker, setDisplayColorPicker] = useState(false);
-  const [eraseMode, setEraseMode] = useState(false);
+  const [displayColorPicker, setDisplayColorPicker] = useState<boolean>(false);
+  const [eraseMode, setEraseMode] = useState<boolean>(false);
+  const [weightChange, setWeightChange] = useState<boolean>(false);
   const [color, setColor] = useState<RGBColor>(DEFAULT_COLOR);
+  const [weight, setWeight] = useState<number>(DEFAULT_WEIGHT);
 
   const handleClick = useCallback(() => {
     setDisplayColorPicker((prev) => !prev);
@@ -34,6 +40,12 @@ export default function Template() {
     },
     [color]
   );
+  const changeWeight = useCallback(
+    (ctx: CanvasRenderingContext2D) => {
+      ctx.lineWidth = weight;
+    },
+    [weight]
+  );
   useEffect(() => {
     if (canvasRef.current !== null) {
       const canvas = canvasRef.current;
@@ -44,7 +56,7 @@ export default function Template() {
       if (ctx !== null) {
         const customCtx = ctx;
         customCtx.lineJoin = "round";
-        customCtx.lineWidth = 1.5;
+        changeWeight(customCtx);
         changeColor(customCtx);
         setGetCtx(ctx);
       }
@@ -63,8 +75,15 @@ export default function Template() {
     }
   }, [color, eraseMode]);
 
+  useEffect(() => {
+    if (getCtx !== undefined) {
+      const customCtx = getCtx;
+      changeWeight(customCtx);
+      setGetCtx(customCtx);
+    }
+  }, [weight]);
+
   const drawFn = (event: React.SyntheticEvent<HTMLCanvasElement, MouseEvent>) => {
-    console.log(event.nativeEvent);
     const mouseX = event.nativeEvent.offsetX;
     const mouseY = event.nativeEvent.offsetY;
 
@@ -80,6 +99,20 @@ export default function Template() {
     setEraseMode((prev) => !prev);
     setDisplayColorPicker(false);
   };
+
+  const clicked = (e: React.SyntheticEvent<HTMLDivElement, MouseEvent>) => {
+    console.log(e.currentTarget === weghtModaltestRef.current);
+    setWeightChange((prev) => !prev);
+    setDisplayColorPicker(false);
+  };
+  const onChangeNumberWeight = (e: any) => {
+    const numbering = Number(e.target.value);
+    setWeight(numbering);
+  };
+  const onChangeRangeWeight = (e: any) => {
+    const numbering = Number(e.target.value);
+    setWeight(numbering);
+  };
   return (
     <Container>
       <Toolbar>
@@ -94,6 +127,18 @@ export default function Template() {
         <EraserBox onClick={transformToEraseMode} changeColor={eraseMode ? "red" : "white"}>
           <BiEraser />
         </EraserBox>
+        <WeightSettingBox ref={weghtModalRef}>
+          <WeightButton onClick={clicked} changeColor={weightChange ? "red" : "white"}>
+            <MdLineWeight />
+          </WeightButton>
+
+          {weightChange ? (
+            <WeightSetting ref={weghtModaltestRef}>
+              <WeightRange type="range" onChange={onChangeRangeWeight} value={weight} />
+              <WeightText type="type" onChange={onChangeNumberWeight} value={weight} />
+            </WeightSetting>
+          ) : null}
+        </WeightSettingBox>
       </Toolbar>
 
       <Canvas
@@ -106,6 +151,80 @@ export default function Template() {
     </Container>
   );
 }
+
+const WeightRange = styled.input`
+  width: 130px;
+`;
+const WeightText = styled.input`
+  width: 30px;
+  -webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  -webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+const widthAnimation = keyframes`
+  0%{
+    width: 0px;
+  }
+  100%{
+    width: 200px;
+  }
+`;
+
+const WeightSetting = styled.div`
+  position: absolute;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  top: 0%;
+  left: 150%;
+  width: 200px;
+  height: 45px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  animation: ${widthAnimation} 0.2s linear;
+  :hover {
+    cursor: default;
+  }
+`;
+
+const WeightSettingBox = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 25px;
+  height: 25px;
+  font-size: 23px;
+  border-radius: 50%;
+  transition: 0.3s all;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const WeightButton = styled.div<ModColor>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  font-size: 25px;
+  border-radius: 50%;
+  background-color: ${(props) => props.changeColor};
+  transition: 0.3s all;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
 const EraserBox = styled.div<ModColor>`
   position: relative;
   display: flex;
@@ -162,6 +281,7 @@ const Toolbar = styled.div`
   position: absolute;
   left: 10px;
   display: flex;
+  justify-content: space-around;
   flex-direction: column;
   align-items: center;
   width: 40px;
